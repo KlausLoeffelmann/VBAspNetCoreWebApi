81e53cfa832b662e40434511c50a1dd3b7741e24
Klaus Löffelmann <klaus@loeffelmann.de>
2017-06-01 14:45:01 +0200

    Initial checkin.

---
+++ src/VbAspNetCore/Controller/ValuesController.vb
@@ -0,0 +1,43 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Threading.Tasks
+Imports Microsoft.AspNetCore.Authorization
+Imports Microsoft.AspNetCore.Mvc
+
+Namespace FirstCSharpWebApp.Controllers
+    <Authorize, Route("api/[controller]")>
+    Public Class ValuesController
+        Inherits Controller
+
+        ' GET api/values
+        <HttpGet>
+        Public Function [Get]() As IEnumerable(Of String)
+            Return New String() {"value1", "value2"}
+        End Function
+
+        ' GET api/values/5
+        <HttpGet("{id}")>
+        Public Function [Get](ByVal id As Integer) As String
+            Return "value"
+        End Function
+
+        ' POST api/values
+        <HttpPost>
+        Public Sub Post(<FromBody> ByVal value As String)
+            ' For more information on protecting this API from Cross Site Request Forgery (CSRF) attacks, see https://go.microsoft.com/fwlink/?LinkID=717803
+        End Sub
+
+        ' PUT api/values/5
+        <HttpPut("{id}")>
+        Public Sub Put(ByVal id As Integer, <FromBody> ByVal value As String)
+            ' For more information on protecting this API from Cross Site Request Forgery (CSRF) attacks, see https://go.microsoft.com/fwlink/?LinkID=717803
+        End Sub
+
+        ' DELETE api/values/5
+        <HttpDelete("{id}")>
+        Public Sub Delete(ByVal id As Integer)
+            ' For more information on protecting this API from Cross Site Request Forgery (CSRF) attacks, see https://go.microsoft.com/fwlink/?LinkID=717803
+        End Sub
+    End Class
+End Namespace
---
+++ src/VbAspNetCore/Program.vb
@@ -0,0 +1,17 @@
+Imports System
+Imports System.IO
+Imports Microsoft.AspNetCore.Hosting
+
+Module Program
+    Sub Main(args As String())
+        Dim host = New WebHostBuilder().
+        UseKestrel().
+        UseContentRoot(Directory.GetCurrentDirectory()).
+        UseIISIntegration().
+        UseStartup(Of StartUp).
+        UseApplicationInsights().
+        Build()
+
+        host.Run()
+    End Sub
+End Module
